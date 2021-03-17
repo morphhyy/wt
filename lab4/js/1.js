42bc378f272c156c2bacd700b3868206f2f70b66
morphhyy <imbibek05@gmail.com>
2021-03-18 00:03:32 +0545

    This is Lab4

---
+++ lab4/js/1.js
@@ -0,0 +1,10 @@
+var num = Number(prompt("Enter a number"))
+
+if(num%2===0){
+	console.log("The input is even");
+}else{
+	console.log("The input is odd")
+}
+
+
+
---
+++ lab4/js/4.js
@@ -0,0 +1,5 @@
+var words = prompt("Enter any words:")
+
+for(var i=words.length-1; i>=0; i--){
+    console.log(words[i])
+}
